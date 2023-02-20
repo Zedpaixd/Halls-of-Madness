5aceb9206e8194ab88fb08dbabdedff18e5531e4
Armand Alexandru Balint <planta636@gmail.com>
2023-02-20 18:29:17 +0200

    merging and transition

--- Assets/Scripts/TriggerSceneSwap.cs
+++ Assets/Scripts/TriggerSceneSwap.cs
@@ -6,15 +6,20 @@ public class TriggerSceneSwap : MonoBehaviour
 {
 
     [SerializeField] private AsyncLoading AL;
+    [SerializeField] private AlternateSight AS;
 
     private void OnTriggerEnter(Collider other)
     {
         if (!other.gameObject.layer.Equals(6))
-            AL.startLoad = 1;
+        {
+            AS.abilityGotten = false;
+            AL.startLoad += 1;
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
         AL.teleport = true;
+        AS.abilityGotten = true;
     }
 }
