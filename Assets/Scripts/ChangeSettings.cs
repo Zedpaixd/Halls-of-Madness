f7fd5f06ef0883213b37bdec71c7a25dcde6eceb
Oliver Vesa <olpertti@gmail.com>
2023-03-04 20:17:36 +0200

    Pause screen settings

--- Assets/Scripts/ChangeSettings.cs
+++ Assets/Scripts/ChangeSettings.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.Rendering.PostProcessing;
 
 public class ChangeSettings : MonoBehaviour
 {
+    public PostProcessProfile brightness;
+    public PostProcessLayer layer;
+    AutoExposure exposure;
+    public AudioSource playerAudioSource;
+    private void Start()
+    {
+        brightness.TryGetSettings(out exposure);
+    }
     public void SetMasterVolume(float value)
     {
         Settings.masterVolume = value;
+        playerAudioSource.volume = value*Settings.sfxVolume;
     }
     public void SetSfxVolume(float value)
     {
         Settings.sfxVolume = value;
+        playerAudioSource.volume = value*Settings.masterVolume;
     }
     public void SetAmbianceVolume(float value)
     {
@@ -20,5 +30,6 @@ public class ChangeSettings : MonoBehaviour
     public void SetBrightness(float value)
     {
         Settings.brightness = value;
+        exposure.keyValue.value = 0.1f*Mathf.Pow(100,value);
     }
 }
